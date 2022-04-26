5b4da41aee806cd5117b60fcc1f3c27984b50c7e
Marcos <socram174@hotmail.com>
2022-04-25 22:27:09 -0400

    Registro de clientes en pagina principal

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -11,7 +11,6 @@
                     <form method="POST" action="/register">
                         @csrf
 
-
                         <div class="row mb-3">
                             <label for="rut" class="col-md-4 col-form-label text-md-end">{{ __('Rut') }}</label>
 
