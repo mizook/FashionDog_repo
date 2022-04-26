3f3186dc2c7a411df4d7cec86ee1a2168ea69be4
mizook <cairamgo@gmail.com>
2022-04-26 00:57:14 -0400

    Se fusiona con marcos2 - Plantilla cliente/Registro en index

--- resources/views/dashboards/client.blade.php
+++ resources/views/dashboards/client.blade.php
@@ -1,13 +1,69 @@
-@extends('layouts.app')
+<!-- Core theme CSS (includes Bootstrap)-->
+<link href="css/styles.css" rel="stylesheet" />
 
-@section('content')
-<body>
+<link href="{{ asset('css/navbar.css') }}" rel="stylesheet">
+<script src="{{ asset('js/navbar.js') }}"></script>
+<nav class="nav">
+    <div class="container">
+        <div class="logo">
+            <img src="{{ asset('assets/img/FashionDogLogo.png') }}" alt="Logo fashion dog" style="width: 25%"><a href="/">Fashion Dog</a>
+        </div>
+        <div id="mainListDiv" class="main_list">
+            <ul class="navlinks">
+                <li><a href="#">About</a></li>
+                <li><a href="#">Cambiar Datos</a></li>
+                <li><a href="#">Profile</a></li>
+                <form action="{{route('logout')}}" method="POST">
+                    @csrf
+                    <button type="submit" class="btn btn-danger">
+                        {{ __('Cerrar sesión') }}
+                    </button>
+                </form>
+            </ul>
+        </div>
+        <span class="navTrigger">
+            <i></i>
+            <i></i>
+            <i></i>
+        </span>
+    </div>
+</nav>
 
-    <h1>Client Dashboard Plantilla</h1>
-    <div>Client RUT: {{ Auth::user()->rut }}</div>
-    <div>Client Name: {{ Auth::user()->name }}</div>
-    <div>Client Last Name: {{ Auth::user()->last_name }}</div>
+<section class="home">
+    <div>
+        <h1>Bienvenido {{Auth::user()->name}}!</h1>
+    </div>
+</section>
+<div style="height: 1000px">
+    <!-- just to make scrolling effect possible -->
+        <h2 class="myH2">What is this ?</h2>
+        <p class="myP">This is a responsive fixed navbar animated on scroll</p>
+        <p class="myP">I took inspiration from  ABDO STEIF (<a href="https://codepen.io/abdosteif/pen/bRoyMb?editors=1100">https://codepen.io/abdosteif/pen/bRoyMb?editors=1100</a>)
+        and Dicson <a href="https://codepen.io/dicson/pen/waKPgQ">(https://codepen.io/dicson/pen/waKPgQ)</a></p>
+        <p class="myP">I HOPE YOU FIND THIS USEFULL</p>
+        <p class="myP">Albi</p>
+            <p class="myP">
 
-</body>
-</html>
-@endsection
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum ratione facere animi impedit rem labore sint repellendus ipsa sapiente voluptatem aut excepturi quo itaque, ab earum cumque. Voluptatem beatae id inventore quod voluptate qui deserunt, quis placeat, tempora ex totam, dolore sequi harum eos voluptatibus animi labore officiis minus laboriosam
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum ratione facere animi impedit rem labore sint repellendus ipsa sapiente voluptatem aut excepturi quo itaque, ab earum cumque. Voluptatem beatae id inventore quod voluptate qui deserunt, quis placeat, tempora ex totam, dolore sequi harum eos voluptatibus animi labore officiis minus laboriosam
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum ratione facere animi impedit rem labore sint repellendus ipsa sapiente voluptatem aut excepturi quo itaque, ab earum cumque. Voluptatem beatae id inventore quod voluptate qui deserunt, quis placeat, tempora ex totam, dolore sequi harum eos voluptatibus animi labore officiis minus laboriosam
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum ratione facere animi impedit rem labore sint repellendus ipsa sapiente voluptatem aut excepturi quo itaque, ab earum cumque. Voluptatem beatae id inventore quod voluptate qui deserunt, quis placeat, tempora ex totam, dolore sequi harum eos voluptatibus animi labore officiis minus laboriosam
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ipsum ratione facere animi impedit rem labore sint repellendus ipsa sapiente voluptatem aut excepturi quo itaque, ab earum cumque. Voluptatem beatae id inventore quod voluptate qui deserunt, quis placeat, tempora ex totam, dolore sequi harum eos voluptatibus animi labore officiis minus laboriosam
+        </p>
+</div>
+
+<!-- Jquery needed -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="js/scripts.js"></script>
+
+<!-- Function used to shrink nav bar removing paddings and adding black background -->
+<script>
+    $(window).scroll(function() {
+        if ($(document).scrollTop() > 50) {
+            $('.nav').addClass('affix');
+            console.log("OK");
+        } else {
+            $('.nav').removeClass('affix');
+        }
+    });
+</script>
