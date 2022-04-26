82291a907c6205c4fa32cca8f8dd22cb27f02122
Sebastian huanca <sebastian.huanca@alumnos.ucn.cl>
2022-04-25 22:19:22 -0400

    Inicio0.2

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
     <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
@@ -18,22 +19,24 @@
         <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
     </head>
 
-    <body id="page-top">
+
+    <body id="page-top" style=" background-color:#8DD7BF">
         <!-- Navigation-->
-        <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top">
+        <nav class="navbar navbar-expand-lg navbar-dark navbar-custom fixed-top" style="background-color: white">
             <div class="container ">
-                <a class="navbar-brand" href="#page-top">Bienvenido a Fashion dog</a>
+                 <img src="assets/img/04.jpg" alt="" style="width: 50px">
+                <a style="color: black" class="navbar-brand" href="#page-top">Bienvenido a Fashion dog</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     @if (Route::has('login'))
                     <div class="navbar-nav ms-auto">
                         @auth
-                          <button type="button" class="btn"> <a href="{{ url('/home') }}" class="button ">Inicio </a></button>
+                          <button type="button"  class="btn"> <a href="{{ url('/home') }}" class="button ">Inicio </a></button>
 
                         @else
-                            <button type="button" class="btn"><a href="{{ route('login') }}" class="button ">Iniciar sesión</button>
+                            <button type="button"  class="btn"><a href="{{ route('login') }}" class="button " style="color: black">Iniciar sesión</button>
                             @if (Route::has('register'))
-                            <button type="button" class="btn"><a href="{{ route('register') }}" class="button ">Registrarse</a></button>
+                            <button type="button"  class="btn"><a href="{{ route('register') }}" class="button " style="color: black">Registrarse</a></button>
 
                     @endif
                         @endauth
@@ -43,66 +46,62 @@
             </div>
         </nav>
         <!-- Header-->
-        <header class="masthead text-center text-white">
-            <div class="masthead-content">
+        <header class="masthead text-center text-white" style="background-color:#8DD7BF" >
+            <div class="masthead-content"  >
                 <div class="container px-5">
                     <h1 class="masthead-heading mb-0">Fashion Dog</h1>
                     <h2 class="masthead-subheading mb-0">Un perro a la moda</h2>
-                    <a class="btn btn-primary btn-xl rounded-pill mt-5" href="#scroll">Más</a>
+                    <a class="btn btn-primary btn-xl rounded-pill mt-5"  href="#scroll">Más</a>
                 </div>
             </div>
-            <div class="bg-circle-1 bg-circle"></div>
-            <div class="bg-circle-2 bg-circle"></div>
-            <div class="bg-circle-3 bg-circle"></div>
-            <div class="bg-circle-4 bg-circle"></div>
         </header>
         <!-- Content section 1-->
-        <section id="scroll">
-            <div class="container px-5">
-                <div class="row gx-5 align-items-center">
-                    <div class="col-lg-6 order-lg-2">
-                        <div class="p-5"><img class="img-fluid rounded-circle" src="assets/img/01.jpg" alt="..." /></div>
-                    </div>
-                    <div class="col-lg-6 order-lg-1">
-                        <div class="p-5">
-                            <h2 class="display-4">Para los amantes de los perros...</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
-                        </div>
-                    </div>
+<section id="scroll">
+    <div class="container px-5">
+        <div class="row gx-5 align-items-center">
+            <div class="col-lg-6 order-lg-2">
+                <div class="p-5"><img class="img-fluid rounded-circle" src="assets/img/01.jpg" alt="..." /></div>
+            </div>
+            <div class="col-lg-6 order-lg-1">
+                <div class="p-5">
+                    <h2 class="display-4">Para los amantes de los perros...</h2>
+                    <p>Si quieres que tu perro pase un buen momento esta es tu solucion</p>
                 </div>
             </div>
-        </section>
-        <!-- Content section 2-->
-        <section>
-            <div class="container px-5">
-                <div class="row gx-5 align-items-center">
-                    <div class="col-lg-6">
-                        <div class="p-5"><img class="img-fluid rounded-circle" src="assets/img/02.jpg" alt="..." /></div>
-                    </div>
-                    <div class="col-lg-6">
-                        <div class="p-5">
-                            <h2 class="display-4">Entregamos el mejor cuidado!</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
-                        </div>
-                    </div>
+        </div>
+    </div>
+</section>
+<!-- Content section 2-->
+<section>
+    <div class="container px-5">
+        <div class="row gx-5 align-items-center">
+            <div class="col-lg-6">
+                <div class="p-5"><img class="img-fluid rounded-circle" src="assets/img/02.jpg" alt="..." /></div>
+            </div>
+            <div class="col-lg-6">
+                <div class="p-5">
+                    <h2 class="display-4">Entregamos el mejor cuidado!</h2>
+                    <p>Con los mejores profesionales al alcance de tu mano.</p>
                 </div>
             </div>
-        </section>
-        <!-- Content section 3-->
-        <section>
-            <div class="container px-5">
-                <div class="row gx-5 align-items-center">
-                    <div class="col-lg-6 order-lg-2">
-                        <div class="p-5"><img class="img-fluid rounded-circle" src="assets/img/03.jpg" alt="..." /></div>
-                    </div>
-                    <div class="col-lg-6 order-lg-1">
-                        <div class="p-5">
-                            <h2 class="display-4">Nuevo estilo!</h2>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quod aliquid, mollitia odio veniam sit iste esse assumenda amet aperiam exercitationem, ea animi blanditiis recusandae! Ratione voluptatum molestiae adipisci, beatae obcaecati.</p>
-                        </div>
-                    </div>
+        </div>
+    </div>
+</section>
+<!-- Content section 3-->
+<section>
+    <div class="container px-5">
+        <div class="row gx-5 align-items-center">
+            <div class="col-lg-6 order-lg-2">
+                <div class="p-5"><img class="img-fluid rounded-circle" src="assets/img/03.jpg" alt="..." /></div>
+            </div>
+            <div class="col-lg-6 order-lg-1">
+                <div class="p-5">
+                    <h2 class="display-4">Experiencia pensada para el disfrute de tu mascota</h2>
+                    <p>Que tu mascota no se estrese mas , aqui lo trataremos como un verdadero rey.</p>
                 </div>
             </div>
+        </div>
+    </div>
         </section>
         <!-- Footer-->
         <footer class="py-5 bg-black">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -23,9 +23,8 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'FashionDog') }}
-                </a>
+
+                <img src="assets/img/04.jpg" alt="" style="width: 50px">
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
