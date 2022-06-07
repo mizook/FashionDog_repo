0ae8898f4889901548b1f4741e18340a9e7ec06f
sebastian.huanca@alumnos.ucn.cl <sebastian.huanca@alumnos.ucn.cl>
2022-06-07 17:38:10 -0400

    MERGEReq

--- resources/views/dashboards/cliente/add_request.blade.php
+++ resources/views/dashboards/cliente/add_request.blade.php
@@ -10,23 +10,27 @@
 
     <div class="content">
 
-    <section class="p-3">
+        <section class="p-3">
             <h1>Solicitar nuevo servicio</h1>
 
             <div class="container">
                 <div class="row justify-content-center">
                     <div class="col-md-8">
                         <div class="card">
-                            <div class="card-header" style="background-color: #ff828b">{{'Escoge la fecha y hora en que deseas recibir el servicio' }}</div>
+                            <div class="card-header" style="background-color: #ff828b">
+                                {{ 'Escoge la fecha y hora en que deseas recibir el servicio' }}</div>
 
                             <div class="card-body">
-                                <form id="createRequestForm" method="POST" action="{{ route('add.request.post', ['rut'=>Auth::user()->rut]) }}">
+                                <form id="createRequestForm" method="POST"
+                                    action="{{ route('add.request.post', ['rut' => Auth::user()->rut]) }}">
                                     @csrf
 
                                     <div class="row form-group">
-                                        <label for="input_date" class ="col-md-4 col-form-label text-md-end">{{ __('Fecha') }}</label>
-                                        <div class="col-sm-6"> 
-                                            <input id="input_date" type="datetime-local" class="form-control @error('input_date') is-invalid @enderror"
+                                        <label for="input_date"
+                                            class="col-md-4 col-form-label text-md-end">{{ __('Fecha') }}</label>
+                                        <div class="col-sm-6">
+                                            <input id="input_date" type="datetime-local"
+                                                class="form-control @error('input_date') is-invalid @enderror"
                                                 name="input_date" required>
 
                                             @error('input_date')
@@ -38,9 +42,11 @@
                                     </div>
 
                                     <div class="row form-group">
-                                        <label for="input_time" class ="col-md-4 col-form-label text-md-end">{{ __('Hora') }}</label>
-                                        <div class="col-sm-6"> 
-                                            <input id="input_time" type="datetime-local" class="form-control @error('input_date') is-invalid @enderror"
+                                        <label for="input_time"
+                                            class="col-md-4 col-form-label text-md-end">{{ __('Hora') }}</label>
+                                        <div class="col-sm-6">
+                                            <input id="input_time" type="datetime-local"
+                                                class="form-control @error('input_date') is-invalid @enderror"
                                                 name="input_time" required>
 
                                             @error('input_time')
@@ -53,7 +59,9 @@
 
                                     <div class="row mb-0">
                                         <div class="col-md-6 offset-md-4">
-                                            <button type="submit" class="btn btn-dark" style="background-color: #ff828b" onclick="ConfirmationPopUp('createRequestForm')">{{ __('Confirmar') }}</button>
+                                            <button type="submit" class="btn btn-dark"
+                                                style="background-color: #ff828b"
+                                                onclick="ConfirmationPopUp('createRequestForm')">{{ __('Confirmar') }}</button>
                                         </div>
                                     </div>
                                     <div>
@@ -69,45 +77,59 @@
 
         </section>
     </div>
-<!-- flatpickr -->
-<script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+    <!-- flatpickr -->
+    <script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
 
-<script>
-    config = {
-        mode: "range",
-        minDate: new Date().fp_incr(1),
-        dateFormat: "Y-m-d",
-        altInput: true,
-        altFormat: "d/m/Y"
-    }
+    <script>
+        config = {
+            mode: "range",
+            minDate: new Date().fp_incr(1),
+            dateFormat: "Y-m-d",
+            altInput: true,
+            altFormat: "d/m/Y",
+            locale: {
+                firstDayOfWeek: 1,
+                weekdays: {
+                    shorthand: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa'],
+                    longhand: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
+                },
+                months: {
+                    shorthand: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Оct', 'Nov', 'Dic'],
+                    longhand: ['Enero', 'Febreo', 'Мarzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre',
+                        'Octubre', 'Noviembre', 'Diciembre'
+                    ],
+                },
+            },
+        }
 
-    flatpickr("input[id=input_date]", config);
-</script>
-<script>
-    config = {
-        noCalendar: true,
-        enableTime: true,
-        dateFormat: "H:i",
-        time_24hr: true
-    }
 
-    flatpickr("input[id=input_time]", config);
-</script>
-<!---Alerta de exito o fracaso--->
-@if (session()->has('goodEdit'))
-<script>
-    swal("¡Datos actualizados!", "Datos actualizados con éxito.", "success");
-</script>
-@elseif (session()->has('clientError'))
-<script>
-    swal("¡Error!", "¡El cliente no existe!", "error");
-</script>
-@elseif (session()->has('dateError'))
-<script>
-    swal("¡Error!", "La fecha que intentas escoger ya está agendada!", "error");
-</script>
-@endif
-<!---Alerta de exito o fracaso--->
+        flatpickr("input[id=input_date]", config);
+    </script>
+    <script>
+        config = {
+            noCalendar: true,
+            enableTime: true,
+            dateFormat: "H:i",
+            time_24hr: true
+        }
+
+        flatpickr("input[id=input_time]", config);
+    </script>
+    <!---Alerta de exito o fracaso--->
+    @if (session()->has('goodEdit'))
+        <script>
+            swal("¡Datos actualizados!", "Datos actualizados con éxito.", "success");
+        </script>
+    @elseif (session()->has('clientError'))
+        <script>
+            swal("¡Error!", "¡El cliente no existe!", "error");
+        </script>
+    @elseif (session()->has('dateError'))
+        <script>
+            swal("¡Error!", "La fecha que intentas escoger ya está agendada!", "error");
+        </script>
+    @endif
+    <!---Alerta de exito o fracaso--->
 
 
 </body>
